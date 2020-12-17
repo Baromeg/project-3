b8bf324d98fa153471d81f9af911f202ca3e7d24
baromeg <baromeg@gmail.com>
2020-12-17 19:18:59 +0000

    Map change

--- frontend/components/Map.js
+++ frontend/components/Map.js
@@ -1,26 +1,29 @@
 import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMapPin } from '@fortawesome/free-solid-svg-icons'
+import { far, faEdit, faMapPin } from '@fortawesome/free-solid-svg-icons'
 import MapGL, { Marker } from 'react-map-gl'
 import '../../node_modules/mapbox-gl/dist/mapbox-gl.css'
-
 const Map = (props) => {
-  // console.log(props)
+  console.log(props)
 
   const [viewPort, setViewPort] = useState({
     height: '250px',
+    // width: '57rem',
     width: '52rem',
+
     zoom: 15,
     latitude: props.location.latitude,
     longitude: props.location.longitude
   })
 
   return <MapGL
-    mapboxApiAccessToken={process.env.MapBoxKey}
+    mapboxApiAccessToken={'pk.eyJ1Ijoibmlja2hheWVzIiwiYSI6ImNrYmh2dW56NDA5ZnIyenB2MHJ4MGFnaWYifQ.IHXzZRvdxBtuH9Ro6nLKmQ'}
+    // mapboxApiAccessToken={process.env.MapBoxKey}
     scrollZoom={false}
     {...viewPort}
     onViewportChange={(viewPort) => setViewPort(viewPort)}
   >
+
     <Marker
       latitude={props.location.latitude}
       longitude={props.location.longitude}
@@ -28,6 +31,7 @@ const Map = (props) => {
       <div>
         <FontAwesomeIcon color="green" icon={faMapPin} />
         <span> {props.location.name} </span>
+
       </div>
     </Marker>
   </MapGL >
